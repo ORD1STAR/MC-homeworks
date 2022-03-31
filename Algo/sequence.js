796917b60f53646b3fd7a80c87a3adc766aea4a3
BuildTools <unconfigured@null.spigotmc.org>
2022-03-31 16:10:13 +0100

    Update sequence.js

--- Algo/sequence.js
+++ Algo/sequence.js
@@ -1,6 +1,8 @@
 function U(n){
-    if(n<=2){
-        return n;
+    if(n>=0){
+        if(n<=2){
+            return n;
+        }
+        return 2*U(n-1)+3*U(n-3);
     }
-    return 2*U(n-1)+3*U(n-3);
 }
